cdd463f14854874352659f8c52d94216162ada71
manuelvpardoo <115119736+manuelvpardoo@users.noreply.github.com>
2023-01-31 14:38:23 -0700

    ...

--- example3.2/script.js
+++ example3.2/script.js
@@ -18,7 +18,7 @@ function init() {
     scene = new THREE.Scene()
     scene.background = new THREE.Color(1,1,1)
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
-    camera.position.y = - 100
+    camera.position.y = - 90
 
     // create the renderer and add it to the html
     renderer = new THREE.WebGLRenderer( { antialias: true } )
